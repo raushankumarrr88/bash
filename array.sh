c976046f4fd2ad143372461490b4462c061f2744
raushanchaudhary840.rc <raushanchaudhary840.rc@gmail.com>
2025-01-11 14:47:49 +0530

    bash

--- array.sh
+++ array.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #array
-
+# for initialization of arrray
 myarray=(1 2 4 5 6 "raushan" helllo)
 
+
+#feaching the value based on index
 echo "first is ${myarray[0]}"
+
+#printing all the values for it use *
 echo "all the values in array is ${myarray[*]}"
