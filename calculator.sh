32be78b5c9029e21471b7b054f303c83700a64e0
raushanchaudhary840.rc <raushanchaudhary840.rc@gmail.com>
2025-01-13 13:18:28 +0530

    1st project

---
+++ calculator.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo "this is a Calculator"
+echo "Press A for add"
+echo "Press B for Subs"
+echo "Press c For mul"
+echo "Press D for Div"
+
+
+function addition {
+    local num1=$1
+    local num2=$2
+    let sum=$num1+$num2
+    echo "the sum of $num1 and $num2 is $sum"
+}
+function substract {
+    local num1=$1
+    local num2=$2
+    let sub=$num1-$num2
+    echo "the Sub of $num1 and $num2 is $sub"
+
+}
+function mul {
+    local num1=$1
+    local num2=$2
+    let mul=$num1*$num2
+    echo "the mul of $num1 and $num2 is $mul"
+
+}
+function div {
+    local num1=$1
+    local num2=$2
+    let div=$num1/$num2
+    echo "the Div of $num1 and $num2 is $div"
+
+}
+read -p "Enter the choice from above" choice
+echo "choice is $choice"
+char=${choice,,}
+echo "Transformed choice is $char"
+case $char in
+    a)
+        echo "You chose Addition"
+        addition $1 $2;;
+    b)
+        echo "You choose to substract"
+        substract $1 $2;;
+    c)
+        echo "you choose to mul"
+        mul $1 $2;;
+    d)
+        echo "you choose to div"
+        div $1 $2;;
+    *)
+        echo "Please enter a valid input"
+esac
+
+
+
---
+++ function.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#to make a function
+# function myfun {
+#     echo "HI"
+
+# }
+
+# #to call a function
+# myfun
+# myfun
+
+
+
+function addition {
+    echo "Welcome $1"
+}
+
+addition $1
